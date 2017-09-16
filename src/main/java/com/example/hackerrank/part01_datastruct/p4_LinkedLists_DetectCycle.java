681e616c33d388e5cee874c72c88a0aa5f6adb61
Carlos Pereira <carlos_pereira@ultimatesoftware.com>
2017-09-16 00:12:47 -0400

    hacker rank: detect list cycle

---
+++ src/main/java/com/example/hackerrank/part01_datastruct/p4_LinkedLists_DetectCycle.java
@@ -0,0 +1,33 @@
+package com.example.hackerrank.part01_datastruct;
+
+import java.util.*;
+import java.io.*;
+
+/**
+ * problem: https://www.hackerrank.com/challenges/ctci-linked-list-cycle
+ * guide: https://www.youtube.com/watch?v=MFOAbpfrJ8g
+ */
+class DetectCycle {
+  boolean hasCycle(Node head) {
+    if (head == null || head.next == null) {
+      return false;
+    }
+
+    Node slow = head;
+    Node fast = head;
+
+    while (fast.next != null && fast.next.next != null) {
+      slow = slow.next;
+      fast = fast.next.next;
+      if (slow == fast) {
+        return true;
+      }
+    }
+    return false;
+  }
+}
+
+class Node {
+  int data;
+  Node next;
+}
