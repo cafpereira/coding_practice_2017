67cd9d51f293e747f09dc19ee3c6b84a135828ee
cpereira <cpereira@groupon.com>
2017-09-19 22:37:12 -0400

    DP Book: Target Sum Problem

--- src/main/java/com/example/dynamicprog_book/Pg39_TargetSum_BottomUp.java
+++ src/main/java/com/example/dynamicprog_book/Pg39_TargetSum_BottomUp.java
@@ -1,7 +1,7 @@
 package com.example.dynamicprog_book;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.io.*;
+import java.util.*;
 
 /**
  * Given an array of integers, nums and a target value T,
@@ -13,28 +13,33 @@ import java.util.Map;
  * targetSum(nums, T) = 5
  */
 public class Pg39_TargetSum_BottomUp {
-    // Top down dynamic programming solution. Like
-    // 0-1 Knapsack, we use a HashMap to save space
     public int targetSum(int[] nums, int T) {
-        // Map: i -> sum -> value
-        int RANGE = 99;
-        if (T < RANGE || T > RANGE) {
-            return 0;
-        }
-        int[][] cache = new int[nums.length][2 * RANGE];
-        for (int i = 0; i < nums.length; i++) {
-            for (int run = -RANGE; run <= RANGE; run++) {
-                if (i == 0) {
-                    if (nums[i] == run) {
-                        cache[i][run] = 1;
-                    }
-                } else {
-                    int add = cache[i - 1][run - nums[i]];
-                    int sub = cache[i - 1][run + nums[i]];
-                    cache[i][run] = add + sub;
+        int sum = 0;
+
+        // Our cache has to range from -sum(nums) to
+        // sum(nums), so we offset everything by sum
+        for (int num : nums) sum += num;
+
+        int[][] cache = new int[nums.length + 1][2 * sum + 1];
+        if (sum == 0) return 0;
+
+        // Initialize i=0, T=0, remember that array is offset by sum
+        // cache[0][sum] is the starting (0,0) coordinate
+        cache[0][sum] = 1;
+
+        // Iterate over previous row and update the current row
+        // This means that instead of assinging to current cache[i]
+        // the sum and subtraction of num[i-1], we increment any
+        // existent value on cache[i+1] with the value of cache[i].
+        for (int i = 1; i <= nums.length; i++) {
+            for (int j = 0; j < 2 * sum + 1; j++) { // equivalent of -sum <= j < sum
+                int prev = cache[i - 1][j];
+                if (prev != 0) {
+                    cache[i][j - nums[i - 1]] += prev;
+                    cache[i][j + nums[i - 1]] += prev;
                 }
             }
         }
-        return cache[nums.length][T];
+        return cache[nums.length][sum + T];
     }
 }
