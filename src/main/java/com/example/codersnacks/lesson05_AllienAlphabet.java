773b3ef4f05220bab17b534e0bce09b37a4bd0b5
Carlos Pereira <carlos_pereira@ultimatesoftware.com>
2017-09-21 23:45:04 -0400

    code snacks lesson 05: alien alphabet

--- src/main/java/com/example/codersnacks/lesson05_AllienAlphabet.java
+++ src/main/java/com/example/codersnacks/lesson05_AllienAlphabet.java
@@ -1,8 +1,40 @@
 package com.example.codersnacks;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 class AlienAlphabet {
+
+  public static List<String> getAlphabetOrder(String[] words) {
+    Set<List<Character>> relations = getRelationships(words);
+    GraphA graph =  GraphA.buildGraph(words, relations);
+    int alphabetLen = graph.keySet().size();
+
+    List<String> order = new ArrayList<>();
+    while (order.size() < alphabetLen) {
+      // Compute topological ordering by iteratively removing
+      // nodes whitout outgoing edges from the graph
+      List<String> letters = nodesWithoutOutgoingEdges(graph);
+      if (letters.size() != 1) {
+        // if size > 1, there are multiple solutions
+        // and size == 0 the graph contains a loop
+        return null;
+      }
+      String next = letters.get(0);
+      graph.removeNode(next);
+
+      order.add(next);
+    }
+    return order;
+  }
+
+  private static List<String> nodesWithoutOutgoingEdges(GraphA graph) {
+    return graph.entrySet()
+        .stream()
+        .filter(e -> e.getValue().adj.isEmpty())
+        .map(Map.Entry::getKey)
+        .collect(Collectors.toList());
+  }
 
   public static Set<List<Character>> getRelationships(String[] words) {
     Set<List<Character>> rel = new HashSet<>();
@@ -22,10 +54,65 @@ class AlienAlphabet {
   }
 
   public static void main(String[] args) {
-    System.out.println("# Expected: (c,a) (b,d) (d,a) (d,c)");
-    System.out.println(getRelationships(new String[]{"bcca", "babc", "dadc", "aad", "aac"}));
-    System.out.println("");
-    System.out.println("# Expected: (a,b)");
-    System.out.println(getRelationships(new String[]{"a", "b", "ba", "bb"}));
+    expect("b, d, c, a", new String[]{"bcca", "babc", "dadc", "aad", "aac"});
+    expect("None", new String[]{"abc"});
+    expect("None", new String[]{"a", "ac", "aa", "b", "c"});
+  }
+
+  private static void expect(String expected, String[] words) {
+    System.out.println("getRelationships(words) = " + getAlphabetOrder(words) + " # Expected: "+ expected);
+  }
+}
+
+class GraphA extends HashMap<String, VertexA> {
+
+  public static GraphA buildGraph(String[] words, Set<List<Character>> edges) {
+    GraphA g = new GraphA();
+    for (String w : words) {
+      for (char c : w.toCharArray()){
+        g.getOrCreate(String.valueOf(c));
+      }
+    }
+    for (List<Character> e : edges){
+//      String from = String.valueOf(e.get(0));
+//      String to = String.valueOf(e.get(1));
+      String from = String.valueOf(e.get(1));
+      String to = String.valueOf(e.get(0));
+      g.getOrCreate(from).append(to);
+      g.getOrCreate(to);
+    }
+    return g;
+  }
+
+  public VertexA getOrCreate(String id) {
+    if (!this.containsKey(id)) {
+      VertexA v = new VertexA(id);
+      this.put(id, v);
+    }
+    return this.get(id);
+  }
+
+  public void removeNode(String node) {
+    for (Map.Entry<String, VertexA> e : entrySet()) {
+      e.getValue().adj.remove(node);
+    }
+    this.remove(node);
+  }
+}
+
+class VertexA {
+  String id;
+  List<String> adj = new ArrayList<>();
+
+  public VertexA(String id) {
+    this.id = id;
+  }
+  public void append(String vertex) {
+    adj.add(vertex);
+  }
+
+  @Override
+  public String toString() {
+    return "{'id':'"+id+"', 'adj': " + adj + "}";
   }
 }
