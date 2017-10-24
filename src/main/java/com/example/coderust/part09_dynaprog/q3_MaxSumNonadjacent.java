860960aa77ea45fa478ae82f7b59e571b0fcbe05
Carlos Pereira <carlos_pereira@ultimatesoftware.com>
2017-10-24 14:39:43 -0400

    coderust part9 q03: Max Sum Nonadjacent Elements

---
+++ src/main/java/com/example/coderust/part09_dynaprog/q3_MaxSumNonadjacent.java
@@ -0,0 +1,23 @@
+package com.example.coderust.part09_dynaprog;
+
+public class q3_MaxSumNonadjacent {
+
+  public static int maxSumNonAdj (int[] array) {
+    int inclusive = 0;
+    int exclusive = 0;
+    int max = 0;
+
+    for (int num : array) {
+      int withoutNum = inclusive;
+      inclusive = Math.max(inclusive, exclusive + num);
+      max = Math.max(max, Math.max(inclusive, exclusive));
+      exclusive = withoutNum;
+    }
+    return max;
+  }
+
+  public static void main(String[] args) {
+    System.out.println("maxSumNonAdj(array) = " + maxSumNonAdj(new int[]{4,1,1,4,2,1}) + " # Expected: " + 9);
+    System.out.println("maxSumNonAdj(array) = " + maxSumNonAdj(new int[]{1,6,10,14,-5,-1,2,-1,3}) + " # Expected: " + 25);
+  }
+}
