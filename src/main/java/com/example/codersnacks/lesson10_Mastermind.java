f51a5dd6bf6832daad69c02f82e794209e578bde
Carlos Pereira <carlos_pereira@ultimatesoftware.com>
2017-10-30 01:09:25 -0400

    improve mastermind solution

--- src/main/java/com/example/codersnacks/lesson10_Mastermind.java
+++ src/main/java/com/example/codersnacks/lesson10_Mastermind.java
@@ -4,8 +4,6 @@ import java.util.*;
 
 class Mastermind {
 
-  public static final char NONE = '\0';
-
   public static int[] checkGuess(String a, String g) {
     char[] answer = a.toCharArray();
     char[] guess = g.toCharArray();
@@ -18,8 +16,6 @@ class Mastermind {
     for (int i = 0; i < guess.length; i++) {
       if (answer[i] == guess[i]){
         hits++;
-        answer[i] = NONE;
-        guess[i] = NONE;
       }
       else {
         ansCount.put(answer[i], ansCount.containsKey(answer[i]) ? ansCount.get(answer[i]) + 1 : 1);
@@ -27,12 +23,16 @@ class Mastermind {
       }
     }
 
+    // Only iterate over guess colors since there is no need
+    // to count near misses for colors that were not used in
+    // the player's guess
     for (Character color : gssCount.keySet()) {
       if (ansCount.containsKey(color)) {
+        // Near miss count is equal the lesser between guess
+        // count and solution count (not including hits).
         near += Math.min(ansCount.get(color), gssCount.get(color));
       }
     }
-
     return new int[]{hits, near};
   }
 
@@ -40,6 +40,9 @@ class Mastermind {
     expect("OOBB", "OBYY", new int[]{1, 1});
     expect("YYYY", "YRRR", new int[]{1, 0});
     expect("YYYR", "OOOY", new int[]{0, 1});
+    expect("RYBO", "RYBO", new int[]{4, 0});
+    expect("RYBO", "OBYR", new int[]{0, 4});
+    expect("OYOY", "YOOY", new int[]{2, 2});
   }
 
   public static void expect(String answer, String guess, int[] expected){
